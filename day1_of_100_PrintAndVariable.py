883dcb638842e94e9ba1fe21a27081ad90b0ef94
AlvinChin <alvin2.chin@live.uwe.ac.uk>
2024-04-28 22:28:16 +0800

    first commit

---
+++ day1_of_100_PrintAndVariable.py
@@ -0,0 +1,29 @@
+# Write your code below this line 👇
+print("Hello World")
+print('She said: "Hello" and then left.')
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
+
+print("Hello World!\nHello World!\nHello World!")
+print("Hello" + " " + "Alvin")
+
+# the computer will process the input before the print function
+print("Hello " + input("what is your name?"))
+
+numOfLetters = len("Angela")
+print(numOfLetters)
+
+name = len(input())
+print(name)
+
+# There are two variables, a and b from input
+a = input()
+b = input()
+# switch the values of a and b
+temp = a
+a = b
+b = temp
+
+print("a: " + a)
+print("b: " + b)
